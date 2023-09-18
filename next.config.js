4ba6ae99db32463a333ffb5428151822f8f63b9a
BoFo <brunofazio466@gmail.com>
2023-09-18 17:35:02 -0300

    Initial commit from Create Next App

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    //nueva forma rutas
+    experimental: {
+        appDir: true,
+    },
+    reactStrictMode: true,
+}
+
+module.exports = nextConfig
